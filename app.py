c02fb6ad6ffafa6879cf9d40be1126f46180fd5d
yaroslav.koval <ksvarog2004@gmail.com>
2022-01-22 23:40:26 +0200

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 from flask_socketio import SocketIO, send
 
 from utils.GridJson import GridJson
@@ -8,6 +9,7 @@ from utils.GridJson import GridJson
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SOCKET_SECRET")
 socketio = SocketIO(app, cors_allowed_origins="https://okx.koval.page")
+CORS(app)
 
 
 @socketio.on('data')
