d85f37d14c49edf24a8a71f4ba470d816a3c600f
yaroslav.koval <ksvarog2004@gmail.com>
2022-01-22 23:04:36 +0200

    migrate to SocketIO

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_socketio import SocketIO, emit
 
-from network.recaptcha import Recaptcha
 from utils.GridJson import GridJson
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-api = Api(app)
-CORS(app)
+app.config['SECRET_KEY'] = 'EAWn3Tbyih4tpyuYCjZ1'
+socketio = SocketIO(app)
 
 
-class DataJson(Resource):
-    @staticmethod
-    def post() -> jsonify:
-        if Recaptcha(request.get_json()["re_token"]).check_token():
-            return GridJson().get()
+@socketio.on('OkxData')
+def handle_my_custom_event(data):
+    return emit(
+        "OkxData", GridJson().get(),
+        namespace="/data", broadcast=True
+    )
 
-
-api.add_resource(DataJson, '/')
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app)
