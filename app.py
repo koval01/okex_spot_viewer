0e50927f2da5cc317accade8002f5b1143b067aa
yaroslav.koval <ksvarog2004@gmail.com>
2022-01-23 12:30:39 +0200

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import logging
 import os
 
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 
@@ -11,15 +12,35 @@ app.config['SECRET_KEY'] = os.getenv("SOCKET_SECRET")
 socketio = SocketIO(app, cors_allowed_origins="https://okx.koval.page")
 CORS(app)
 
+clients = []
+
 
 @socketio.on('connect')
-def connection_(msg):
-    emit('connection', {'data': 'Connected'})
+def handle_connect():
+    logging.info('Client connected')
+    clients.append(request.sid)
 
 
-@socketio.on('message')
-def handleMessage(msg):
-    return emit("message", GridJson().get(), broadcast=True)
+@socketio.on('disconnect')
+def handle_disconnect():
+    logging.info('Client disconnected')
+    clients.remove(request.sid)
+
+
+def send_message(client_id):
+    emit('message', GridJson().get(), room=client_id)
+
+
+# def main():
+#     webapp_thread = threading.Thread(target=run_web_app)
+#     webapp_thread.start()
+#
+#     while not clients:
+#         logging.info("waiting for client to connect")
+#         sleep(1)
+#
+#     sleep(1)
+#     send_message(clients[0])
 
 
 if __name__ == "__main__":
