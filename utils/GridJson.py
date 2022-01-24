235eae2c055a9677ef694f2b34694dcdc4a0b10b
yaroslav.koval <ksvarog2004@gmail.com>
2022-01-24 18:04:02 +0200

    update

--- utils/GridJson.py
+++ utils/GridJson.py
@@ -15,7 +15,9 @@ class GridJson:
         self.hints = json.loads(open(
             os.path.dirname(os.path.abspath(__file__)) + '/../other/hints.json', "r"
         ).read())["hint"]
-        self.uah_price = CurrencyGet().get()
+        self.currency = {
+            "uah_price": CurrencyGet().get()
+        }
         self.trades_data = TradesGrid().get()
         self.grid_data = OkxApi().get()
 
@@ -27,27 +29,19 @@ class GridJson:
         return [{
             "trade_time": int(el.tradeTime),
             "profit": self.data_round(el.totalPnl),
-            "profit_uah": self.data_round(el.totalPnl) * self.uah_price
         } for el in ModelTrades(**self.trades_data).data]
 
     def build_grid_data(self) -> dict:
         data = ModelGrid(**self.grid_data).data[0]
-        uah_price = self.uah_price
         currency = ExchangeModel(**ExchangeData(data.instId).get()).data[0]
         return {
             "algo_id": OkxApi().short_id(data.algoId),
             "annualized_rate": round(float(data.annualizedRate), 3),
-            "annualized_rate_uah": round(float(data.annualizedRate) * uah_price, 3),
             "profit": round(float(data.gridProfit), 3),
-            "profit_uah": round(float(data.gridProfit) * uah_price, 3),
             "current_price": round(float(currency.last), 3),
-            "current_price_uah": round(float(currency.last) * uah_price, 3),
             "float_profit": round(float(data.floatProfit), 3),
-            "float_profit_uah": round(float(data.floatProfit) * uah_price, 3),
             "total_price": round(float(data.totalPnl), 3),
-            "total_price_uah": round(float(data.totalPnl) * uah_price, 3),
             "run-price": round(float(data.runPx), 3),
-            "run-price_uah": round(float(data.runPx) * uah_price, 3),
             "trades_num": int(data.tradeNum),
             "arbitrages_num": int(data.arbitrageNum),
             "created_at_utc": int(data.cTime),
@@ -65,7 +59,8 @@ class GridJson:
                 "success": len(self.grid_data["data"]) > 0 and len(data_trades) > 0,
                 "trades": data_trades,
                 "data": data_grid,
-                "hint": self.hints
+                "hint": self.hints,
+                "currency": self.currency,
             }
         except Exception as e:
             return {"success": False, "exception": str(e)}
